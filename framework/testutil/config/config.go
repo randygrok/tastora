7dae729606060443311321e468d694a1afb65197
Cian Hatton <github.qpeyb@simplelogin.fr>
2025-06-30 11:45:31 +0100

    feat: update config marshalling to support mapstructure for toml files (#58)

--- framework/testutil/config/config.go
+++ framework/testutil/config/config.go
@@ -5,7 +5,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/BurntSushi/toml"
+	"github.com/mitchellh/mapstructure"
 	"path/filepath"
+	"reflect"
+	"strconv"
 	"strings"
 )
 
@@ -63,7 +66,24 @@ func unmarshalByExtension(data []byte, config interface{}, configPath string) er
 
 	switch filepath.Ext(configPath) {
 	case ".toml":
-		return toml.Unmarshal(data, config)
+		// the toml files like app.toml and config.toml use mapstructure annotations, not toml annotations.
+		// so we need to decode with the mapstructure library.
+		var tomlMap map[string]interface{}
+		if err := toml.Unmarshal(data, &tomlMap); err != nil {
+			return err
+		}
+		decoder, err := mapstructure.NewDecoder(&mapstructure.DecoderConfig{
+			DecodeHook: mapstructure.ComposeDecodeHookFunc(
+				mapstructure.StringToTimeDurationHookFunc(),
+				mapstructure.StringToSliceHookFunc(","),
+				stringConversionHook(),
+			),
+			Result: config,
+		})
+		if err != nil {
+			return err
+		}
+		return decoder.Decode(tomlMap)
 	case ".json":
 		return json.Unmarshal(data, config)
 	default:
@@ -84,10 +104,67 @@ func marshalByExtension(config interface{}, filePath string) ([]byte, error) {
 	ext := strings.ToLower(filepath.Ext(filePath))
 	switch ext {
 	case ".toml":
-		return toml.Marshal(config)
+		var tomlMap map[string]interface{}
+		decoder, err := mapstructure.NewDecoder(&mapstructure.DecoderConfig{
+			Result: &tomlMap,
+		})
+		if err != nil {
+			return nil, err
+		}
+		if err := decoder.Decode(config); err != nil {
+			return nil, err
+		}
+		return toml.Marshal(tomlMap)
 	case ".json":
 		return json.MarshalIndent(config, "", "  ")
 	default:
 		return nil, fmt.Errorf("unsupported config file format: %s", ext)
+	}
+}
+
+// stringConversionHook converts string values to int types for mapstructure
+// this is required as some values in the toml files are "2" and "true" and are not
+// parsable by default by mapstructure. This hook handles the conversion logic.
+func stringConversionHook() mapstructure.DecodeHookFunc {
+	return func(f, t reflect.Type, data interface{}) (interface{}, error) {
+		if f.Kind() != reflect.String {
+			return data, nil
+		}
+
+		str := data.(string)
+
+		switch t.Kind() {
+		case reflect.Int:
+			return strconv.Atoi(str)
+		case reflect.Int8:
+			val, err := strconv.ParseInt(str, 10, 8)
+			return int8(val), err
+		case reflect.Int16:
+			val, err := strconv.ParseInt(str, 10, 16)
+			return int16(val), err
+		case reflect.Int32:
+			val, err := strconv.ParseInt(str, 10, 32)
+			return int32(val), err
+		case reflect.Int64:
+			return strconv.ParseInt(str, 10, 64)
+		case reflect.Uint:
+			val, err := strconv.ParseUint(str, 10, 0)
+			return uint(val), err
+		case reflect.Uint8:
+			val, err := strconv.ParseUint(str, 10, 8)
+			return uint8(val), err
+		case reflect.Uint16:
+			val, err := strconv.ParseUint(str, 10, 16)
+			return uint16(val), err
+		case reflect.Uint32:
+			val, err := strconv.ParseUint(str, 10, 32)
+			return uint32(val), err
+		case reflect.Uint64:
+			return strconv.ParseUint(str, 10, 64)
+		case reflect.Bool:
+			return strconv.ParseBool(str)
+		default:
+			return data, nil
+		}
 	}
 }
